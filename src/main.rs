e4d30df4ddf0cf5f3006290530f026c0160077f0
ak2ume <ak2ume@users.noreply.github.com>
2021-10-17 17:29:06 +0900

    receive and display keyboard input

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,16 @@
  * Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
  *-------------------------------------------------------------------------------------------------------------*/
 
+use std::io;
+
 fn main() {
-    let name = "TRPL";
-    println!("Hello, {}!", name);
+    println!("Guess the number!");
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line.");
+
+    println!("You guessd: {}", guess);
 }
